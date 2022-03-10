43a7be6780847f5dd83960a349435deff8b6468b
isnancahyadi <isnan.arifc@gmail.com>
2022-03-10 23:08:59 +0700

    menambahkan proses login, add session, dan redirect ke dashboard dosen

--- app/src/main/java/com/arcmedtek/mpuskaapp_mobile/activity/Login.java
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/activity/Login.java
@@ -5,27 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.arcmedtek.mpuskaapp_mobile.R;
+import com.arcmedtek.mpuskaapp_mobile.activity.user.lecture.LectureDashboard;
+import com.arcmedtek.mpuskaapp_mobile.service.MPuskaDataService;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText _username, _password;
     TextView _btnSignUp;
+    Button _btnLogin;
     ImageView _web, _telegram, _instagram, _twitter, _youtube;
+    LinearLayout _containerSecondContent;
+    ProgressBar _loading;
+
+    public MPuskaDataService mPuskaDataService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        _btnSignUp =findViewById(R.id.btn_sign_up);
+        _username = findViewById(R.id.txt_inet_login_username);
+        _password = findViewById(R.id.txt_inet_login_pass);
+        _btnSignUp = findViewById(R.id.btn_sign_up);
         _web = findViewById(R.id.btn_web);
         _telegram = findViewById(R.id.btn_telegram);
         _instagram = findViewById(R.id.btn_instagram);
         _twitter = findViewById(R.id.btn_twitter);
         _youtube = findViewById(R.id.btn_youtube);
+        _btnLogin = findViewById(R.id.btn_login);
+        _containerSecondContent = findViewById(R.id.container_second_content);
+        _loading = findViewById(R.id.loading_login);
+
+        mPuskaDataService = new MPuskaDataService(Login.this);
+
+        _btnLogin.setOnClickListener(v -> {
+            String mUsername = String.valueOf(_username.getText());
+            String mPassword = String.valueOf(_password.getText());
+
+            if (!mUsername.isEmpty() && !mPassword.isEmpty()) {
+                isLogin(mUsername, mPassword);
+            } else {
+                if (mUsername.isEmpty() && !mPassword.isEmpty()) {
+                    _username.setError("Masukkan NIY/NIM");
+                } else if (!mUsername.isEmpty()) {
+                    _password.setError("Masukkan password");
+                } else {
+                    _username.setError("Masukkan NIY/NIM");
+                    _password.setError("Masukkan password");
+                }
+            }
+        });
 
         _btnSignUp.setOnClickListener(v -> {
             Intent intent = new Intent(Login.this, SignUp.class);
@@ -33,6 +71,31 @@ public class Login extends AppCompatActivity {
         });
 
         _socmed();
+    }
+
+    private void isLogin(String mUsername, String mPassword) {
+        _loading.setVisibility(View.VISIBLE);
+        _containerSecondContent.setVisibility(View.GONE);
+
+        mPuskaDataService.logIn(mUsername, mPassword, new MPuskaDataService.LoginListener() {
+            @Override
+            public void onResponse(String privilege) {
+                if (privilege.equals("1")) {
+                    moveToLectureDashboard();
+                }
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        });
+    }
+
+    private void moveToLectureDashboard() {
+        Intent intent = new Intent(Login.this, LectureDashboard.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     private void _socmed() {
--- app/src/main/java/com/arcmedtek/mpuskaapp_mobile/service/MPuskaDataService.java
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/service/MPuskaDataService.java
@@ -1,16 +1,27 @@
 package com.arcmedtek.mpuskaapp_mobile.service;
 
 import android.content.Context;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.arcmedtek.mpuskaapp_mobile.config.SessionManager;
 import com.arcmedtek.mpuskaapp_mobile.config.SingletonReq;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MPuskaDataService {
     public static final String QUERY_FOR_CREATE_ACCOUNT = "http://100.100.1.10/mpuska-server-side/mpuska-server/public/restapi/akun";
+    public static final String QUERY_FOR_LOGIN = "http://100.100.1.10/mpuska-server-side/mpuska-server/public/restapi/auth/loginProcess";
 
     Context context;
     SessionManager _sessionManager;
@@ -31,6 +42,44 @@ public class MPuskaDataService {
 
     public void signUp(String username, String password, SignUpListener signUpListener) {
         StringRequest request = new StringRequest(Request.Method.POST, QUERY_FOR_CREATE_ACCOUNT, response -> signUpListener.onResponse("Akun berhasil dibuat"), error -> signUpListener.onError(String.valueOf(error.networkResponse.statusCode))) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("password", password);
+                return params;
+            }
+        };
+        SingletonReq.getInstance(context).addToRequestQueue(request);
+    }
+
+    public interface LoginListener {
+        void onResponse(String privilege);
+        void onError(String message);
+    }
+
+    public void logIn(String username, String password, LoginListener loginListener) {
+        StringRequest request = new StringRequest(Request.Method.POST, QUERY_FOR_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                _privilege = "";
+                _user = "";
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    _user = jsonObject.getString("username");
+                    _privilege = jsonObject.getString("hak_akses");
+                    _sessionManager.createSession(_user, _privilege);
+                } catch (JSONException e) {
+                    Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
+                }
+                loginListener.onResponse(_privilege);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(context, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
             @Override
             protected Map<String, String> getParams() {
                 Map<String, String> params = new HashMap<>();
