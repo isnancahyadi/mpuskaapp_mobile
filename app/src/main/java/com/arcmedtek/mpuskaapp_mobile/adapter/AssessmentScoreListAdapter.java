843c3fcfd124abc7422d69f2358bfcac6db6fb95
isnancahyadi <isnan.arifc@gmail.com>
2022-05-26 21:22:05 +0700

    menambahkan conversi nilai pada assessment score

--- app/src/main/java/com/arcmedtek/mpuskaapp_mobile/adapter/AssessmentScoreListAdapter.java
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/adapter/AssessmentScoreListAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -38,6 +39,30 @@ public class AssessmentScoreListAdapter extends RecyclerView.Adapter<AssessmentS
         KhsModel model = _khsModels.get(position);
         holder._assessmentName.setText(model.get_assessment());
         holder._assessmentScore.setText(String.valueOf(model.get_score()));
+
+        if (model.get_score() >= 0 && model.get_score() < 40.00) {
+            holder._grade.setImageResource(R.drawable.ic_grade_e);
+        } else if (model.get_score() >= 40.00 && model.get_score() < 43.75) {
+            holder._grade.setImageResource(R.drawable.ic_grade_d);
+        } else if (model.get_score() >= 43.75 && model.get_score() < 51.25) {
+            holder._grade.setImageResource(R.drawable.ic_grade_d_plus);
+        } else if (model.get_score() >= 51.25 && model.get_score() < 55.00) {
+            holder._grade.setImageResource(R.drawable.ic_grade_c_min);
+        } else if (model.get_score() >= 55.00 && model.get_score() < 57.50) {
+            holder._grade.setImageResource(R.drawable.ic_grade_c);
+        } else if (model.get_score() >= 57.50 && model.get_score() < 62.50) {
+            holder._grade.setImageResource(R.drawable.ic_grade_c_plus);
+        } else if (model.get_score() >= 62.50 && model.get_score() < 65.00) {
+            holder._grade.setImageResource(R.drawable.ic_grade_b_min);
+        } else if (model.get_score() >= 65.00 && model.get_score() < 68.75) {
+            holder._grade.setImageResource(R.drawable.ic_grade_b);
+        } else if (model.get_score() >= 68.75 && model.get_score() < 76.25) {
+            holder._grade.setImageResource(R.drawable.ic_grade_b_plus);
+        } else if (model.get_score() >= 76.25 && model.get_score() < 80.00) {
+            holder._grade.setImageResource(R.drawable.ic_grade_a_min);
+        } else if (model.get_score() >= 80.00 && model.get_score() <= 100.00) {
+            holder._grade.setImageResource(R.drawable.ic_grade_a);
+        }
     }
 
     @Override
@@ -48,12 +73,14 @@ public class AssessmentScoreListAdapter extends RecyclerView.Adapter<AssessmentS
     public static class AssessmentHolder extends RecyclerView.ViewHolder {
 
         TextView _assessmentName, _assessmentScore;
+        ImageView _grade;
 
         public AssessmentHolder(@NonNull View itemView) {
             super(itemView);
 
             _assessmentName = itemView.findViewById(R.id.txt_name_assessment);
             _assessmentScore = itemView.findViewById(R.id.txt_score_assessment);
+            _grade = itemView.findViewById(R.id.grade);
         }
     }
 }
