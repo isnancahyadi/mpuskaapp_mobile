899398b7b617b729811e4d2d06aaddd14c143604
isnancahyadi <isnan.arifc@gmail.com>
2022-08-13 10:45:36 +0700

    memperbaiki tampilan list nilai dan memperbaiki update nilai

---
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/activity/user/lecture/ScoreDetail.java
@@ -0,0 +1,185 @@
+package com.arcmedtek.mpuskaapp_mobile.activity.user.lecture;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.arcmedtek.mpuskaapp_mobile.R;
+import com.arcmedtek.mpuskaapp_mobile.adapter.AssessmentScoreListAdapter;
+import com.arcmedtek.mpuskaapp_mobile.adapter.ChangeScoreListAdapter;
+import com.arcmedtek.mpuskaapp_mobile.config.OnEditTextChanged;
+import com.arcmedtek.mpuskaapp_mobile.model.KhsModel;
+import com.arcmedtek.mpuskaapp_mobile.service.MPuskaDataService;
+
+import java.util.ArrayList;
+
+public class ScoreDetail extends AppCompatActivity {
+
+    TextView _collegeYear, _nim, _studentName, _teamName;
+    RecyclerView _assessmentScoreRecycler, _changeAssessmentScoreRecycler;
+    CardView _btnChangeScore;
+    RelativeLayout _containerListScoreDetail, _containerChangeScoreDetail;
+    Button _btnCancelUpdateScore, _btnSaveUpdateScore;
+
+    String _strCollegeYear, _strNim, _strFirstName, _strMiddleName, _strLastName, _strTeamName, _strIdKrs;
+    String[] _score, _idAssessment;
+
+    MPuskaDataService _mPuskaDataService;
+    AssessmentScoreListAdapter _assessmentScoreListAdapter;
+    ChangeScoreListAdapter _changeScoreListAdapter;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_detail);
+
+        _strCollegeYear = getIntent().getStringExtra("college_year");
+        _strNim = getIntent().getStringExtra("nim");
+        _strFirstName = getIntent().getStringExtra("first_name");
+        _strMiddleName = getIntent().getStringExtra("middle_name");
+        _strLastName = getIntent().getStringExtra("last_name");
+        _strTeamName = getIntent().getStringExtra("team_name");
+        _strIdKrs = getIntent().getStringExtra("ID_krs");
+
+        _collegeYear = findViewById(R.id.txt_college_year_score_detail);
+        _nim = findViewById(R.id.txt_nim_score_detail);
+        _studentName = findViewById(R.id.txt_name_score_detail);
+        _teamName = findViewById(R.id.txt_team_name_score_detail);
+        _assessmentScoreRecycler = findViewById(R.id.score_list);
+        _changeAssessmentScoreRecycler = findViewById(R.id.change_score_list);
+        _containerListScoreDetail = findViewById(R.id.container_list_score_detail);
+        _containerChangeScoreDetail = findViewById(R.id.container_change_score_detail);
+        _btnChangeScore = findViewById(R.id.btn_change_score);
+        _btnCancelUpdateScore = findViewById(R.id.btn_cancel_update_assessments);
+        _btnSaveUpdateScore = findViewById(R.id.btn_save_update_assessments);
+
+        _mPuskaDataService = new MPuskaDataService(ScoreDetail.this);
+
+        _collegeYear.setText(_strCollegeYear);
+        _nim.setText(_strNim);
+        if (_strMiddleName.equals("")) {
+            _studentName.setText(_strFirstName + " " + _strLastName);
+        } else {
+            _studentName.setText(_strFirstName + " " + _strMiddleName + " " + _strLastName);
+        }
+        _teamName.setText(_strTeamName);
+
+        studentList(_strIdKrs);
+    }
+
+    private void studentList(String strIdKrs) {
+        _mPuskaDataService.getStudentScore(strIdKrs, new MPuskaDataService.StudentScoreListener() {
+            @Override
+            public void onResponse(ArrayList<KhsModel> khsModels) {
+                _assessmentScoreListAdapter = new AssessmentScoreListAdapter(khsModels, ScoreDetail.this);
+                _assessmentScoreRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+                _assessmentScoreRecycler.setAdapter(_assessmentScoreListAdapter);
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        });
+
+        _btnChangeScore.setOnClickListener(v -> {
+            changeStudentScore(strIdKrs);
+        });
+    }
+
+    private void changeStudentScore(String strIdKrs) {
+        _containerListScoreDetail.setVisibility(View.GONE);
+        _containerChangeScoreDetail.setVisibility(View.VISIBLE);
+
+        _mPuskaDataService.getStudentScore(strIdKrs, new MPuskaDataService.StudentScoreListener() {
+            @Override
+            public void onResponse(ArrayList<KhsModel> khsModels) {
+                _score = new String[khsModels.size()];
+                _idAssessment = new String[khsModels.size()];
+
+                for (int i = 0; i < khsModels.size(); i++) {
+                    _idAssessment[i] = String.valueOf(khsModels.get(i).get_idAsesmen());
+                    _score[i] = String.valueOf(khsModels.get(i).get_score());
+                }
+
+                _changeScoreListAdapter = new ChangeScoreListAdapter(khsModels, ScoreDetail.this, strIdKrs, new OnEditTextChanged() {
+                    @Override
+                    public void onTextChanged(int position, String charSeq) {
+                        _score[position] = charSeq;
+                    }
+
+                    @Override
+                    public void beforeTextChanged(int position, String charSeq) {
+                        _score[position] = charSeq;
+                    }
+                });
+
+                _changeAssessmentScoreRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                _changeAssessmentScoreRecycler.setAdapter(_changeScoreListAdapter);
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        });
+
+        _btnSaveUpdateScore.setOnClickListener(v -> {
+            updateScore(_score, _idAssessment, strIdKrs);
+        });
+
+        _btnCancelUpdateScore.setOnClickListener(v -> refreshActivity());
+    }
+
+    private void updateScore(String[] score, String[] idAssessment, String strIdKrs) {
+        _mPuskaDataService.updateScoreMhs(strIdKrs, idAssessment, score, new MPuskaDataService.UpdateScoreMhs() {
+            @Override
+            public void onResponse(String message) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ScoreDetail.this, R.style.AlertDialogStyle);
+                View doneDialog = LayoutInflater.from(ScoreDetail.this).inflate(R.layout.custom_done_dialog, findViewById(R.id.confirm_done_dialog));
+                builder.setView(doneDialog);
+
+                TextView txtMessage = doneDialog.findViewById(R.id.done_message);
+                txtMessage.setText(message);
+
+                final AlertDialog alertDialog = builder.create();
+
+                doneDialog.findViewById(R.id.btn_confirm_done).setOnClickListener(v -> {
+                    refreshActivity();
+                });
+
+                if (alertDialog.getWindow() != null) {
+                    alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+
+                alertDialog.show();
+            }
+
+            @Override
+            public void onError(String message) {
+                Toast.makeText(ScoreDetail.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void refreshActivity() {
+        finish();
+        overridePendingTransition(0, 0);
+        startActivity(getIntent());
+        overridePendingTransition(0, 0);
+    }
+}
--- app/src/main/java/com/arcmedtek/mpuskaapp_mobile/adapter/InputValueAdapter.java
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/adapter/InputValueAdapter.java
@@ -21,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.arcmedtek.mpuskaapp_mobile.R;
 import com.arcmedtek.mpuskaapp_mobile.activity.user.lecture.ChangeScore;
+import com.arcmedtek.mpuskaapp_mobile.activity.user.lecture.ScoreDetail;
 import com.arcmedtek.mpuskaapp_mobile.model.KhsModel;
 import com.arcmedtek.mpuskaapp_mobile.model.KrsModel;
 import com.arcmedtek.mpuskaapp_mobile.service.MPuskaDataService;
@@ -54,7 +55,12 @@ public class InputValueAdapter extends RecyclerView.Adapter<InputValueAdapter.In
     public void onBindViewHolder(@NonNull InputValueHolder holder, int position) {
         KrsModel model = _krsModels.get(position);
         holder._nim.setText(model.get_nim());
-        holder._studentName.setText(model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
+
+        if (model.get_studentMiddleName().equals("")) {
+            holder._studentName.setText(model.get_studentFirstName() + " " + model.get_studentLastName());
+        } else {
+            holder._studentName.setText(model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
+        }
 
         MPuskaDataService _mPuskaDataService;
         _mPuskaDataService = new MPuskaDataService(_context);
@@ -122,61 +128,70 @@ public class InputValueAdapter extends RecyclerView.Adapter<InputValueAdapter.In
         });
 
         holder.itemView.setOnClickListener(v -> {
-            Dialog dialogInput = new Dialog(v.getContext());
-            dialogInput.setContentView(R.layout.input_score);
-
-            TextView nim, name, teamName;
-            ImageView btnClose;
-            Button btnChangeScore;
-            RecyclerView assessmentScoreRecycler;
-            MPuskaDataService mPuskaDataService;
-
-            mPuskaDataService = new MPuskaDataService(_context);
-
-            nim = dialogInput.findViewById(R.id.txt_nim_input_score);
-            name = dialogInput.findViewById(R.id.txt_name_input_score);
-            teamName = dialogInput.findViewById(R.id.txt_team_name_input_score);
-            btnClose = dialogInput.findViewById(R.id.btn_close_input);
-            btnChangeScore = dialogInput.findViewById(R.id.btn_change_score);
-            assessmentScoreRecycler = dialogInput.findViewById(R.id.assessment_score_list);
-
-            nim.setText(model.get_nim());
-            name.setText(model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
-            teamName.setText(model.get_teamName());
-
-            mPuskaDataService.getStudentScore(String.valueOf(model.get_idKrs()), new MPuskaDataService.StudentScoreListener() {
-                @Override
-                public void onResponse(ArrayList<KhsModel> khsModels) {
-                    AssessmentScoreListAdapter assessmentScoreListAdapter;
-                    assessmentScoreListAdapter = new AssessmentScoreListAdapter(khsModels, _context);
-                    assessmentScoreRecycler.setLayoutManager(new LinearLayoutManager(_context.getApplicationContext()));
-
-                    assessmentScoreRecycler.setAdapter(assessmentScoreListAdapter);
-                }
-
-                @Override
-                public void onError(String message) {
-
-                }
-            });
-
-            btnChangeScore.setOnClickListener(v1 -> {
-                Intent intent = new Intent(_context, ChangeScore.class);
-                intent.putExtra("ID_krs", String.valueOf(model.get_idKrs()));
-                intent.putExtra("college_year", _strCollegeYear);
-                intent.putExtra("nim", model.get_nim());
-                intent.putExtra("student_name", model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
-                intent.putExtra("team_name", model.get_teamName());
-                _context.startActivity(intent);
-                dialogInput.dismiss();
-            });
-
-            btnClose.setOnClickListener(v2 -> {
-                dialogInput.dismiss();
-            });
-
-            dialogInput.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-            dialogInput.show();
+            Intent intent = new Intent(_context, ScoreDetail.class);
+            intent.putExtra("ID_krs", String.valueOf(model.get_idKrs()));
+            intent.putExtra("nim", String.valueOf(model.get_nim()));
+            intent.putExtra("first_name", String.valueOf(model.get_studentFirstName()));
+            intent.putExtra("middle_name", String.valueOf(model.get_studentMiddleName()));
+            intent.putExtra("last_name", String.valueOf(model.get_studentLastName()));
+            intent.putExtra("team_name", String.valueOf(model.get_teamName()));
+            intent.putExtra("college_year", _strCollegeYear);
+            _context.startActivity(intent);
+//            Dialog dialogInput = new Dialog(v.getContext());
+//            dialogInput.setContentView(R.layout.input_score);
+//
+//            TextView nim, name, teamName;
+//            ImageView btnClose;
+//            Button btnChangeScore;
+//            RecyclerView assessmentScoreRecycler;
+//            MPuskaDataService mPuskaDataService;
+//
+//            mPuskaDataService = new MPuskaDataService(_context);
+//
+//            nim = dialogInput.findViewById(R.id.txt_nim_input_score);
+//            name = dialogInput.findViewById(R.id.txt_name_input_score);
+//            teamName = dialogInput.findViewById(R.id.txt_team_name_input_score);
+//            btnClose = dialogInput.findViewById(R.id.btn_close_input);
+//            btnChangeScore = dialogInput.findViewById(R.id.btn_change_score);
+//            assessmentScoreRecycler = dialogInput.findViewById(R.id.assessment_score_list);
+//
+//            nim.setText(model.get_nim());
+//            name.setText(model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
+//            teamName.setText(model.get_teamName());
+//
+//            mPuskaDataService.getStudentScore(String.valueOf(model.get_idKrs()), new MPuskaDataService.StudentScoreListener() {
+//                @Override
+//                public void onResponse(ArrayList<KhsModel> khsModels) {
+//                    AssessmentScoreListAdapter assessmentScoreListAdapter;
+//                    assessmentScoreListAdapter = new AssessmentScoreListAdapter(khsModels, _context);
+//                    assessmentScoreRecycler.setLayoutManager(new LinearLayoutManager(_context.getApplicationContext()));
+//
+//                    assessmentScoreRecycler.setAdapter(assessmentScoreListAdapter);
+//                }
+//
+//                @Override
+//                public void onError(String message) {
+//
+//                }
+//            });
+//
+//            btnChangeScore.setOnClickListener(v1 -> {
+//                Intent intent = new Intent(_context, ChangeScore.class);
+//                intent.putExtra("ID_krs", String.valueOf(model.get_idKrs()));
+//                intent.putExtra("college_year", _strCollegeYear);
+//                intent.putExtra("nim", model.get_nim());
+//                intent.putExtra("student_name", model.get_studentFirstName() + " " + model.get_studentMiddleName() + " " + model.get_studentLastName());
+//                intent.putExtra("team_name", model.get_teamName());
+//                _context.startActivity(intent);
+//                dialogInput.dismiss();
+//            });
+//
+//            btnClose.setOnClickListener(v2 -> {
+//                dialogInput.dismiss();
+//            });
+//
+//            dialogInput.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+//            dialogInput.show();
         });
     }
 
