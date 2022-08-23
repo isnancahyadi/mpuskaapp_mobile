76e1dd5c8beb639cbfdb537b5461f1cd21b588eb
isnancahyadi <isnan.arifc@gmail.com>
2022-08-23 18:20:30 +0700

    memperbaiki UI login

--- app/src/main/java/com/arcmedtek/mpuskaapp_mobile/service/MPuskaDataService.java
+++ app/src/main/java/com/arcmedtek/mpuskaapp_mobile/service/MPuskaDataService.java
@@ -30,7 +30,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MPuskaDataService {
-    public static final String IPCONF = "192.168.1.103";
+    public static final String IPCONF = "192.168.1.16";
     public static final String BASE_URL = "http://" + IPCONF + "/mpuska-server-side/mpuska-server/public/restapi/";
 
     public static final String QUERY_FOR_LOGIN = BASE_URL + "auth/loginProcess";
